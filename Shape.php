e7d228314bebdb8d6efd6e41423ba873daee0f19
LePhuong96 <bigankroi@gmail.com>
2019-04-08 18:14:08 +0700

    first commit

---
+++ Shape.php
@@ -0,0 +1,12 @@
+<?php
+class Shape{
+    public $name;
+
+    public function __construct()
+    {
+        $this->name =$name;
+    }
+    public function show(){
+        return "I am s shape. My name is $this->name";
+    }
+}
